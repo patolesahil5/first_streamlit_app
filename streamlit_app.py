1a2290dab1471dc714d586f6d904c2ced47f1b30
patolesahil5 <71871488+patolesahil5@users.noreply.github.com>
2023-04-27 14:27:48 +0530

    Update streamlit_app.py
    made changes in the 28th line and added a new line to make a choice and a textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,14 @@ streamlit.dataframe(fruits_to_show);
 
 # New section to display Fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalizing the response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Output it the screen as a table.
 streamlit.dataframe(fruityvice_normalized)
+
